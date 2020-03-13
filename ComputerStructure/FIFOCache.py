3a291b1a38d489a0d54cb39aaccb433b4623e84a
JamesLiuwenliang <13114817178@163.com>
2020-03-13 20:22:58 +0900

    FIFOCache is created.

--- ComputerStructure/FIFOCache.py
+++ ComputerStructure/FIFOCache.py
@@ -125,29 +125,55 @@ class DoubleLinkedList :
                 line += '->'
         print(line)
 
-if __name__ == '__main__':
-    l = DoubleLinkedList(10)
-    nodes = []
-    for i in range(10):
-        node = Node(i, i)
-        nodes.append(node)
 
-    l.append(nodes[0])
-    l.print()
-    l.append(nodes[1])
-    l.print()
-    l.pop()
-    l.print()
-    l.append(nodes[2])
-    l.print()
-    l.append_front(nodes[3])
-    l.print()
-    l.append(nodes[4])
-    l.print()
-    l.remove(nodes[2])
-    l.print()
-    l.remove()
-    l.print()
-       
+class FIFOCache(object):
+    def __init__(self,capacity):
+        self.capacity = capacity;
+        self.size = 0;
+        self.map = {}
+        self.list = DoubleLinkedList(self.capacity)
+
+    def get(self,key):
+        if key not in self.map :
+            return -1 
+        else :
+            node = self.map.get(key)
+            return node.value
+    
+    def put(self,key,value):
+        if self.capacity == 0:
+            return 
+        
+        if key in self.map :
+            node = self.map.get(key)
+            self.list.remove(node)
+            node.value = value 
+            self.list.append(node)
+        else:
+            if self.size == self.capacity:
+                node = self.list.pop()
+                del self.map[node.key]
+            node = Node(key,value)
+            self.list.append(node)
+            self.map[key] = node
+            self.size += 1
+
+    def print(self):
+        self.list.print()
+
+if __name__ == '__main__':
+    cache = FIFOCache(2)
+    cache.put(1, 1)
+    cache.print()
+    cache.put(2, 2)
+    cache.print()
+    print(cache.get(1))
+    cache.put(3, 3)
+    cache.print()
+    print(cache.get(2))
+    cache.print()
+    cache.put(4, 4)
+    cache.print()
+    print(cache.get(1))
 
 
