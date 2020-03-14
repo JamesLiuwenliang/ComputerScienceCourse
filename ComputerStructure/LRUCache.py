7e6e0322ed68bcee1220bb931281510aa354fc69
JamesLiuwenliang <13114817178@163.com>
2020-03-14 18:16:40 +0900

    LRUCache is created.

--- ComputerStructure/LRUCache.py
+++ ComputerStructure/LRUCache.py
@@ -124,3 +124,62 @@ class DoubleLinkedList :
             if p:
                 line += '->'
         print(line)
+
+
+
+class LRUCache(object):
+    def __init__(self,capacity):
+        self.capacity = capacity;
+        self.size = 0;
+        self.map = {}
+        self.list = DoubleLinkedList(self.capacity)
+
+    def get(self,key):
+        if key not in self.map:
+            return -1
+        else:
+            node = self.map.get(key)
+            self.list.remove(node)
+            self.list.append_front(node)
+            return node.value
+
+    def put(self,key,value):
+
+        # 节点在Cache中 : 找到节点并更新节点值,节点会进入最前面的位置
+        if key in self.map:
+            node = self.map.get(key)
+            self.list.remove(node)
+            node.value = value
+            self.list.append_front(node)
+        # 节点不在Cache中,需要添加节点
+        else:  
+            
+            node = Node(key,value)
+
+            # 需要考虑缓存满否
+            if(self.list.size >= self.list.capacity):
+                old_node = self.list.remove()
+                self.map.pop(old_node.key)
+
+            self.list.append_front(node)
+            self.map[key] = node
+    
+    def print(self):
+        self.list.print()
+
+if __name__ == '__main__':
+    cache = LRUCache(2)
+    cache.put(2, 2)
+    cache.print()
+    cache.put(1, 1)
+    cache.print()
+    cache.put(3, 3)
+    cache.print()
+
+    print(cache.get(1))
+    cache.print()
+    print(cache.get(2))
+    cache.print()
+
+    print(cache.get(3))
+    cache.print()
